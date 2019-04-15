beaa3716e5c983ae229012baa1418c05c07b928f
fargito <fr.farge96@gmail.com>
2019-04-15 23:00:31 +0200

    implement first pokemon

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -13,7 +13,11 @@ class Home extends React.Component {
           Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des
           pokemons !
         </div>
-        <div>Commence par créer ton premier pokemon: {pokemon}</div>
+        <div>
+          <p>Carapuce</p>
+          <p>Numéro 7</p>
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"/>
+        </div>
       </Style.Intro>
     );
   }
