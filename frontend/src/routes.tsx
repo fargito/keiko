1cd2ecaac9736acce0cb26274fde5cbba1503689
fargito <fr.farge96@gmail.com>
2019-05-02 10:59:25 +0200

    clean reset.css, implement clean GlobalStyle at routes.tsx level

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,13 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router';
+import GlobalStyle from './globalStyle';
 
 const Home = lazy(() => import('./pages/Home'));
 const PokemonPage = lazy(() => import('./pages/PokemonPage'));
 
 const routes = () => (
   <Suspense fallback={<div>Loading...</div>}>
+    <GlobalStyle />
     <Switch>
       <Route exact path="/" component={() => <Redirect to="/pokedex/1" />} />
       <Route exact path="/pokedex/:page" component={Home} />
